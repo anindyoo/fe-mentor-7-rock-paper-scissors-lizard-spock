2d141d67274e1309697af43b591f7b97ba82cefc
anindyoo <anindyo0216@gmail.com>
2024-08-13 19:27:49 +0700

    feat: implement score header ui.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layouts/Main';
+import Home from './pages/Home';
+
 const App = () => {
-  console.log();
+  const routes = [
+    {
+      element: <Main />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+      ],
+    },
+  ];
+
+  const routeConfig = {
+    basename: '/fe-mentor-7-rock-paper-scissors-lizard-spock',
+  };
+
+  const router = createBrowserRouter(routes, routeConfig);
 
   return (
-    <div className="App ">
-      RPSLS
+    <div className="
+    APP-CONTAINER
+    flex flex-col justify-center gap-[2.875rem]
+    pt-[2.875rem]"
+    >
+      <RouterProvider router={router} />
     </div>
   );
 };
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,63 @@
+const titleNames = ['ROCK', 'PAPER', 'SCISSORS', 'LIZARD', 'SPOCK'];
+
+const Header = () => {
+  console.log();
+
+  return (
+    <div className="
+    HEADER
+    SECTION-CENTER
+    flex justify-center"
+    >
+      <div className="
+      SCORE-HEADER
+      flex flex-row justify-between items-center
+      max-w-[44rem] w-full h-[9.625rem]
+      pl-[2.125rem] pr-[1.625rem]
+      border border-headerOutline
+      rounded-2xl"
+      >
+        <ul className="
+        TITLE-BOX
+        flex flex-col gap-[-1.375rem]
+        text-[1.75rem] font-bold"
+        >
+          {titleNames.map((data, index) => (
+            <li
+              key={data}
+              className={`
+              TITLE-NAME
+              ${index > 0 && '-mt-[1.313rem]'}`}
+            >
+              {data}
+            </li>
+          ))}
+        </ul>
+        <div className="
+        SCORE-BOX
+        flex flex-col  items-center
+        max-w-[9.375rem] w-full h-[7.125rem]
+        pt-3
+        rounded-lg
+        bg-scoreBgColor"
+        >
+          <div className="
+          SCORE-TITLE
+          text-[1.063rem] tracking-[0.11rem] font-semibold text-scoreText"
+          >
+            SCORE
+          </div>
+          <div className="
+          SCORE-NUMBER
+          -mt-[0.188rem]
+          text-[3.875rem]/[1.065em] font-bold text-scoreNumberText"
+          >
+            12
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
