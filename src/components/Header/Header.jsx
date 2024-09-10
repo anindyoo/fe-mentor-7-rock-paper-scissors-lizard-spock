1764d1652d6f0cee605dddac98a6c0280e9ca7b5
anindyoo <anindyo0216@gmail.com>
2024-09-10 10:05:06 +0700

    feat: implement mobile stylings for the header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,22 +21,22 @@ const Header = () => {
       <div className="
       SCORE-HEADER
       flex flex-row justify-between items-center
-      max-w-[19.688rem] lg:max-w-[44rem] w-full h-[9.625rem]
-      pl-[2.125rem] pr-[1.625rem]
+      max-w-[19.688rem] lg:max-w-[44rem] w-full h-[6.25rem] lg:h-[9.625rem]
+      p-[0.875rem] lg:pl-[2.125rem] lg:pr-[1.625rem]
       border-4 border-headerOutline
-      rounded-2xl"
+      rounded-lg lg:rounded-2xl"
       >
         <ul className="
         TITLE-BOX
-        flex flex-col gap-[-1.375rem]
-        text-[1.75rem] font-bold"
+        flex flex-col gap-[-0.625rem] lg:gap-[-1.375rem]
+        text-[0.813rem] lg:text-[1.75rem] font-bold"
         >
           {titleNames.map((data, index) => (
             <li
               key={data}
               className={`
               TITLE-NAME
-              ${index > 0 && '-mt-[1.313rem]'}`}
+              ${index > 0 && '-mt-[0.625rem] lg:-mt-[1.313rem]'}`}
             >
               {data}
             </li>
@@ -44,22 +44,27 @@ const Header = () => {
         </ul>
         <div className="
         SCORE-BOX
-        flex flex-col  items-center
-        max-w-[9.375rem] w-full h-[7.125rem]
-        pt-3
-        rounded-lg
+        flex flex-col items-center
+        max-w-20 lg:max-w-[9.375rem] w-full h-[4.5rem] lg:h-[7.125rem]
+        pt-2.5 lg:pt-3
+        rounded-[0.25rem] lg:rounded-lg
         bg-scoreBgColor"
         >
           <div className="
           SCORE-TITLE
-          text-[1.063rem] tracking-[0.11rem] font-semibold text-scoreText"
+          ml-[0.15rem] lg:[0.11rem  ]
+          text-[0.625rem] lg:text-[1.063rem]
+          tracking-[0.15rem] lg:tracking-[0.11rem]
+          font-semibold text-scoreText
+          text-center"
           >
             SCORE
           </div>
           <div className="
           SCORE-NUMBER
           -mt-[0.188rem]
-          text-[3.875rem]/[1.065em] font-bold text-scoreNumberText"
+          text-[2.5rem]/[2.813rem] lg:text-[3.875rem]/[1.065em]
+          font-bold text-scoreNumberText"
           >
             {currentScore}
           </div>
