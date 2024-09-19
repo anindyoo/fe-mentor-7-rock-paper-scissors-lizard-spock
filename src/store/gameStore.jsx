755029026a90a34a0f4222031baf8815fc9ddb31
anindyoo <anindyo0216@gmail.com>
2024-09-19 11:08:40 +0700

    feat: update the states to be persistent.

--- src/store/gameStore.jsx
+++ src/store/gameStore.jsx
@@ -1,45 +1,53 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import helpers from '../helpers/helpers';
 
-const useGameStore = create((set) => ({
-  player1: null,
-  player2: null,
-  score: 0,
-  winner: 0,
-  played: false,
-  setPlayer: ({ player, choice }) => {
-    set(() => (
-      player === 1
-        ? ({ player1: choice })
-        : ({ player2: choice })
-    ));
-  },
-  setScore: () => {
-    set((state) => {
-      const difference = state.player1 - state.player2;
-      if (difference === 0) {
-        return ({ winner: 3 });
-      }
+const useGameStore = create(
+  persist(
+    (set) => ({
+      player1: null,
+      player2: null,
+      score: 0,
+      winner: 0,
+      played: false,
+      setPlayer: ({ player, choice }) => {
+        set(() => (
+          player === 1
+            ? ({ player1: choice })
+            : ({ player2: choice })
+        ));
+      },
+      setScore: () => {
+        set((state) => {
+          const difference = state.player1 - state.player2;
+          if (difference === 0) {
+            return ({ winner: 3 });
+          }
 
-      return (helpers.isOdd(helpers.customModulo((difference), 5)))
-        ? ({
-          winner: 1,
-          score: state.score + 1,
-        })
-        : ({
-          winner: 2,
-          score: state.score - 1 < 0
-            ? 0
-            : state.score - 1,
+          return (helpers.isOdd(helpers.customModulo((difference), 5)))
+            ? ({
+              winner: 1,
+              score: state.score + 1,
+            })
+            : ({
+              winner: 2,
+              score: state.score - 1 < 0
+                ? 0
+                : state.score - 1,
+            });
         });
-    });
-  },
-  resetGame: () => set({
-    player1: null,
-    player2: null,
-    winner: 0,
-  }),
-  setPlayed: () => set({ played: true }),
-}));
+      },
+      resetGame: () => set({
+        player1: null,
+        player2: null,
+        winner: 0,
+      }),
+      setPlayed: () => set({ played: true }),
+    }),
+    {
+      name: 'rock-paper-scissors-ls',
+    },
+  ),
+);
 
 export default useGameStore;
